1f1717a7ccf85a04d4ab2203a48e8bc60bfb79b2
Rony Castejón <60992883+Ronying@users.noreply.github.com>
2022-11-24 16:21:46 -0600

    Add files via upload

---
+++ config/db.php
@@ -0,0 +1,13 @@
+<?php
+
+define('DB_HOST', 'dbaws.c7ckmgn8w9j8.us-east-1.rds.amazonaws.com');
+define('DB_USER', 'admin');
+define('DB_PASS', '12345678');
+define('DB_NAME', 'DBAWS');
+
+define('NOMBRE_EMPRESA', '504MARKET');
+define('DIRECCION_EMPRESA', 'Tegucigalpa, Honduras, C.A.');
+define('TELEFONO_EMPRESA', '+(504) 2222-2220');
+define('EMAIL_EMPRESA', '504market@gmail.com');
+define('RTN_EMPRESA', '123456789');
+define('ISV', '15');
